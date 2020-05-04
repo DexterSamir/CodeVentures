8042be80bd9a2b56fa5e6f5728e1b10f5676723c
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2020-05-05 01:28:31 +0600

    Add files via upload

---
+++ AtCoder/ContestTime!/atcoder164problemA.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+
+{
+
+    int S, W;
+
+    cin>>S>>W;
+
+    cout<<((W<S)? "safe":"unsafe");
+
+}
+
---
+++ AtCoder/ContestTime!/atcoder166problemA.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Author: Ezio Sam
+
+#define ll long long
+
+#define pb push_back 
+
+#define sp " "
+
+int main()
+
+{
+
+	ll n, a, b, c;	
+
+	string s;
+
+	cin>>s;
+
+    
+
+    if(s=="ABC") cout<<"ARC"<<endl ;
+
+    else cout<<"ABC"<<endl ;
+
+   return 0;
+
+}
