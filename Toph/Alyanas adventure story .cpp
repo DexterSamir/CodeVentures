bc31ef039319b7147778cf5c85f026d43d65c6ba
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2020-05-17 21:22:34 +0600

    Add files via upload

---
+++ Toph/Alyanas adventure story .cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Author: Ezio Sam
+/* Shorthands */
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+#define sp " "
+#define fi first
+#define sn second 
+/* STL */
+#define vi vector<int>v
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mem(a, n) memset(a, n, sizeof(a));
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+/* loops */
+#define fr(i,n) for(i=0;i<n;i++)
+#define fn(i,n) for(i=1;i<=n;i++)
+#define fa(i,a,n) for(i=a;i<=n;i++)
+/* inputs */
+#define si(n)               scanf("%d", &n)
+#define sl(n)               scanf("%lld", &n)
+#define sii(a, b)           scanf("%d %d", &a, &b)
+#define sll(a, b)           scanf("%lld %lld", &a, &b)
+#define siii(a, b, c)       scanf("%d %d %d", &a, &b, &c)
+#define slll(a, b, c)       scanf("%lld %lld %lld", &a, &b, &c)
+#define siiii(a, b, c, d)   scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sllll(a, b, c, d)   scanf("%lld %lld %lld %lld", &a, &b, &c, &d)
+   
+#define Ezio ios::sync_with_stdio(0),cin.tie(NULL),cout.tie(NULL);
+#define debugg(something)   cout<<#something<<" - "<<something<<"\n";         
+int main()
+{
+	int n, m, t,  b, c, i, j;
+	
+    si(t);
+    int a[t];
+    fr(i, t)cin>>a[i];
+    
+    cout <<*max_element(a, a+t)<<endl ;
+   return 0;
+}
