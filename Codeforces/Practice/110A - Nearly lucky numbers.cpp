2b5d3e5461169c709f6dcb2b213929f09724bdcf
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2020-06-08 14:25:50 +0600

    Add files via upload

---
+++ Codeforces/Practice/110A - Nearly lucky numbers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Author: Ezio Sam
+/* Shorthands */
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+#define sp ": "
+#define cs "Case "
+#define fi first
+#define sn second 
+#define po(x) x*x
+/* STL */
+#define vi vector<int>v
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mem(a, n) memset(a, n, sizeof(a));
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define mx(v) *max_element(v.begin(),v.end())
+#define mxa(a) *max_element(a, a.size())
+#define r(a) reverse(a.begin(),a.end())
+/* loops */
+#define fr(i,n) for(__typeof(n) i=0;i<(n);i++)
+#define fn(i,n) for(__typeof(n)i=1;i<=(n);i++)
+#define fa(i,a,n) for(__typeof(a)i=(a);i<=(b);i++)
+
+#define Ezio ios::sync_with_stdio(0),cin.tie(NULL),cout.tie(NULL);
+#define db(something)   cout<<#something<<" - "<<something<<"\n";         
+int main()
+{
+   /*Variables */
+   int m, n, p, r, t=0;
+   ll a, b, c, d, e;
+   double x, y, z;
+   string s, ss, sss;
+   
+   /*Code*/
+   cin>>s;
+   
+   fr(i, sz(s)){
+   	if(s[i]=='4' || s[i]=='7' ){
+   		t++;
+   	}
+   }
+   
+   if(t==4 ||t==7)cout<<"YES"<<endl ;
+   else cout<<"NO"<<endl ;
+  
+   
+   return 0;
+}
