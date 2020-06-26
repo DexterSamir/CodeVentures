d5cb750e3c0ac6621431fdf3295e7c21a76c6120
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2020-06-26 10:29:38 +0600

    Add files via upload

---
+++ Codeforces/Practice/1325B CopyCopyCopy.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Author: Ezio Sam
+/* Shorthands */
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+#define sp ": "
+#define cs "Case "
+#define fi first
+#define sn second 
+#define po(x) x*x
+/* STL */
+#define vi vector<int>v
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mem(a,n) memset(a, n, sizeof(a));
+#define all(x) x.begin(),x.end()
+#define alr(x) x.rbegin(),x.rend()
+#define sz(x)  (int)x.size()
+#define mx(v)  *max_element(v.begin(),v.end())
+#define mxa(a) *max_element(a, a.size())
+#define r(a) reverse(a.begin(),a.end())
+/* loops */
+#define fr(i,n)   for(__typeof(n) i=0;i<(n);i++)
+#define fn(i,n)   for(__typeof(n)i=1;i<=(n);i++)
+#define fa(i,a,n) for(int  i=a;i<=b;i++)
+/*Special*/
+#define Ezio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define db(something)  cerr<<#something<<" - "<<something<<"\n";         
+
+int main()
+{
+  
+   Ezio
+   int n,t;
+   cin>>t;
+   while(t--){
+     set<int>st;
+     cin>>n;
+     fr(i,n){
+        int m;
+        cin>>m;
+        st.insert(m);
+     }
+     
+     cout<<sz(st)<<endl;
+     st.clear();
+   }
+   return 0;
+}
+    
---
+++ Codeforces/Practice/1353B Two Array and swap.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Author: Ezio Sam
+/* Shorthands */
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+#define sp ": "
+#define cs "Case "
+#define fi first
+#define sn second 
+#define po(x) x*x
+/* STL */
+#define vi vector<int>v
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mem(a,n) memset(a, n, sizeof(a));
+#define all(x) x.begin(),x.end()
+#define sz(x)  (int)x.size()
+#define mx(v)  *max_element(v.begin(),v.end())
+#define mxa(a) *max_element(a, a.size())
+#define r(a) reverse(a.begin(),a.end())
+/* loops */
+#define fr(i,n)   for(__typeof(n) i=0;i<(n);i++)
+#define fn(i,n)   for(__typeof(n)i=1;i<=(n);i++)
+#define fa(i,a,n) for(__typeof(a)i=(a);i<=(b);i++)
+/*Special*/
+#define Ezio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define db(something)   cerr<<#something<<" - "<<something<<"\n";         
+int main()
+{
+  
+   Ezio
+   int t,n,k;
+   cin>>t;
+   
+   while(t--){
+     cin>>n>>k;
+     vector<int>a(n);
+     vector<int>b(n);
+     
+     for(auto &x:a) cin>>x;
+     for(auto &x:b) cin>>x;
+     
+     sort(all(a));
+     sort(b.rbegin(),b.rend());
+     
+     fr(i,k){
+       a.pb(b[i]);
+     }
+     
+     sort(a.rbegin(),a.rend());
+     
+     int sum=0;
+     fr(i,n){
+       sum+=a[i];
+     }
+     
+     cout<<sum<<endl;
+   }
+   
+   return 0;
+}
+    
