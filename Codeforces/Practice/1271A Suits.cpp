70a8f4a9f575f0f94b20d47c6c6b9b1bd93af4c9
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2020-07-09 11:50:51 +0600

    Add files via upload

---
+++ Codeforces/Practice/1271A Suits.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Author: Ezio Sam
+/* Shorthands */
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+#define sp " "
+#define cs "Case "
+#define fi first
+#define sn second 
+#define po(x) x*x
+/* STL */
+#define vi vector<int>v
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mem(a,n) memset(a, n, sizeof(a));
+#define all(x) x.begin(),x.end()
+#define alr(x) x.rbegin(),x.rend()
+#define sz(x)  (int)x.size()
+#define mx(v)  *max_element(v.begin(),v.end())
+#define mxi(v) distance(v.begin(),max_element(v.begin(),v.end()))
+#define mxa(a) *max_element(a, a.size())
+#define r(a) reverse(a.begin(),a.end())
+/* loops */
+#define fr(i,n)   for(__typeof(n) i=0;i<(n);i++)
+#define fn(i,n)   for(__typeof(n)i=1;i<=(n);i++)
+#define fa(i,a,n) for(int  i=a;i<=b;i++)
+/*Special*/
+#define Ezio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define db(something)  cerr<<#something<<" - "<<something<<"\n";         
+
+int main()
+{
+  
+   Ezio
+   ll t,m,n,x,a,b,c,d;
+   string s;
+   
+   cin>>a>>b>>c>>d>>m>>n;
+   
+   b=min({b,c,d});
+   a=min(a,d);
+   
+   if(a+b>d){
+      if(m>n) b=d-a;
+      else a=d-b;
+   }
+   
+   cout<<a*m + b*n<<endl;
+   
+   return 0;
+}
+    
