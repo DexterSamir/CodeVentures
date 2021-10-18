0eb7861b12f23c73486305d1cc875b2c21c2e5a7
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2021-10-18 20:27:37 +0600

    Update 977. Squares of a Sorted Array.cpp

--- LeetCode/977. Squares of a Sorted Array.cpp
+++ LeetCode/977. Squares of a Sorted Array.cpp
@@ -1,3 +1,38 @@
+//Using Two pointers
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n =  nums.size();
+        vector<int> res(n);
+        
+        int low=0;
+        int high=n-1;
+        
+        for(int i=n-1;i>=0;i--){
+         
+            if(abs(nums[high]) > abs(nums[low])){
+                int x = nums[high] * nums[high];
+                res[i]= x;
+                high--;
+            }
+            
+            else{
+                int x = nums[low] * nums[low];
+                res[i]= x;
+                low++; 
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
+
+
+
 //Normal approach
 /*
 class Solution {
