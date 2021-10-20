dbded7a4ef68dbf5027847d4f2d56eeb1522830b
DexterSamir <64350826+DexterSamir@users.noreply.github.com>
2021-10-20 19:28:48 +0600

    Create 557. Reverse Words in a String III.cpp

---
+++ LeetCode/557. Reverse Words in a String III.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream str(s);
+        char sp= ' ';
+        string r,res;
+        res="";
+        while(getline(str,r,sp)){
+            reverse(r.begin(),r.end());
+            res += r;
+            if()
+            res += sp;
+        }
+        res.pop_back();
+        return res;
+    }
+};
